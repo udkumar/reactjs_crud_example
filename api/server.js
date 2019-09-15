87d19c0a2b36b6606b76d095c243fe4c1af1c596
uday <udkumar@hotmail.com>
2019-09-15 05:48:01 +0530

    added new route for model

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const PORT = 4000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./DB.js');
+const businessRoute = require('./business.route');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
@@ -15,6 +16,8 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use('/business', businessRoute);
 
 app.listen(PORT, function(){
   console.log('Server is running on Port:',PORT);
